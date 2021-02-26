80a1bfa7dbade87bf911f17c93378550f9be9d4f
coffeescript27 <79711985+coffeescript27@users.noreply.github.com>
2021-02-26 10:46:48 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var word1 = window.prompt("adjective");
+var word2 =  window.prompt("type of furniture");
+var word3 =  window.prompt("name");
+var word4 =  window.prompt("number");
+document.write("there  was a very "  +  word1  +  " "  +  word2 + " that a person named " +  word3 + "  bought   for   "   +  word4 + "$"); 
